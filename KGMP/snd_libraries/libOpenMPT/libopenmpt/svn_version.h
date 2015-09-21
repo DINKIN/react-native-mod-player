5073f157d92f56aefddb323e6473bef7490f2311
Stanislav Bershadskiy <stan229@Stanislavs-MBP.home>
2015-09-21 15:13:30 -0400

    NVDSP part 2

---
+++ KGMP/snd_libraries/libOpenMPT/libopenmpt/svn_version.h
@@ -0,0 +1,14 @@
+//
+//  svn_version.h
+//  libopenmpt
+//
+//  Created by Stanislav Bershadskiy on 9/21/15.
+//  Copyright © 2015 DeezNuts. All rights reserved.
+//
+
+#ifndef svn_version_h
+#define svn_version_h
+
+
+#endif /* svn_version_h */
+#define SVN_VERSION "40"
