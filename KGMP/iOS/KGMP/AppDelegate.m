2eaa7fcc2c4ccd46d07cb0a2bc5b5791d0b2efde
Jay Garcia <jesusdgarcia@gmail.com>
2015-09-30 10:34:07 -0400

    migration updates

--- KGMP/iOS/KGMP/AppDelegate.m
+++ KGMP/iOS/KGMP/AppDelegate.m
@@ -30,9 +30,10 @@
    * `inet` value under `en0:`) and make sure your computer and iOS device are
    * on the same Wi-Fi network.
    */
+#if TARGET_IPHONE_SIMULATOR
 
   jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle"];
-
+#else
   /**
    * OPTION 2
    * Load from pre-bundled file on disk. To re-generate the static bundle
@@ -43,6 +44,9 @@
    * see http://facebook.github.io/react-native/docs/runningondevice.html
    */
 
+   jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+
+#endif
 //  
 //    for (id familyName in [UIFont familyNames]) {
 //        NSLog(@"%@", familyName);
@@ -51,7 +55,6 @@
 //        }
 //    }
 
-//   jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"KGMP"
