66c12cd1206b47609c8c24f9bb54c0f06cbeadc1
Jay Garcia <jesusdgarcia@gmail.com>
2016-03-29 11:45:11 -0400

    Updated Scheme to allow background playback. Updated AppDelegate.m, providing the ability for automatic local JS asset loading

--- KGMP/ios/KGMP/AppDelegate.m
+++ KGMP/ios/KGMP/AppDelegate.m
@@ -30,9 +30,11 @@
    * `inet` value under `en0:`) and make sure your computer and iOS device are
    * on the same Wi-Fi network.
    */
+#if TARGET_IPHONE_SIMULATOR
 
   jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios&dev=true"];
-
+  
+#else
   /**
    * OPTION 2
    * Load from pre-bundled file on disk. The static bundle is automatically
@@ -41,8 +43,9 @@
    * simulator in the "Release" build configuration.
    */
 
-//   jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+   jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 
+#endif
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"KGMP"
                                                initialProperties:nil
