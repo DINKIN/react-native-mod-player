16a4e9b5d485f02b7f09d400ebdeecc4340de2f8
Jay Garcia <jesusdgarcia@gmail.com>
2016-05-16 21:13:39 -0400

    Faster loading between tracks

--- KGMP/ios/ModusCreate/MCModPlayer.m
+++ KGMP/ios/ModusCreate/MCModPlayer.m
@@ -13,7 +13,8 @@
     int lastPattern,
         lastRow;
     
-
+    BOOL isLoading,
+         isPaused;
 }
 
 
@@ -108,69 +109,6 @@ struct StatusObject statuses[NUM_BUFFERS];
 }
 
 
-void audioCallback(void *data, AudioQueueRef mQueue, AudioQueueBufferRef mBuffer) {
-
-    
-    MCModPlayer *player = (__bridge MCModPlayer*)data;
-    
-//    pthread_mutex_lock(&audio_mutex);
-    memcpy(mBuffer->mAudioData, audioGenerationBuffer[soundPlayerBufferIndex], mBuffer->mAudioDataByteSize);
-
-    soundGeneratorFlag[soundPlayerBufferIndex] = 0;
-//    printf("RD  \t#%i\t\t%i\n", soundPlayerBufferIndex, abs(soundPlayerBufferIndex - soundGeneratorBufferIndex));   fflush(stdout);
-
-    unsigned long index = soundPlayerBufferIndex;
-    soundPlayerBufferIndex++;
-  
-
-    if (soundPlayerBufferIndex == NUM_BUFFERS) {
-        soundPlayerBufferIndex = 0;
-    }
-    
-    if (player.appActive) {
-        // TODO: Should we use GCD to execute this method in the main queue??
-        
-//         dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), ^{ // 1
-        
-            struct StatusObject status = statuses[index];
-            int32_t playerState[4];
-
-            playerState[0] = status.order;
-            playerState[1] = status.pattern;
-            playerState[2] = status.row;
-            playerState[3] = status.numRows;
-
-//            printf("%i\t%i\n", status.pattern, status.row);
-            [player notifyInterface:playerState];
-
-//            int size = mBuffer->mAudioDataByteSize * sizeof(SInt16);
-//            int size = mBuffer->mAudioDataByteSize;
-        
-//            int numFrames = bufferSize / (2 * sizeof(int16_t));
-
-//            SInt16 *delegateData = malloc(size);
-//            memcpy(delegateData, mBuffer->mAudioData, size);
-        
-            
-//            MCPlotGlViewManager *delegate = [player getDelegate];
-//            [delegate updateBuffers:audioVizBuffer[soundPlayerBufferIndex] withSize:numFrames];
-
-//            [delegate updateLeft:vizGenerationBufferL[soundPlayerBufferIndex] andRight:vizGenerationBufferR[soundPlayerBufferIndex] withNumFrames:numFrames];
-//            free(delegateData);
-        
-
-
-//        });
-    }
-    
-    
-    AudioQueueEnqueueBuffer(mQueue, mBuffer, 0, NULL);
-//    pthread_mutex_unlock(&audio_mutex);
-
-    
-}
-
-
 - (BOOL) initAudioSession {
     AVAudioSession *session = [AVAudioSession sharedInstance];
 
@@ -205,24 +143,31 @@ void audioCallback(void *data, AudioQueueRef mQueue, AudioQueueBufferRef mBuffer
 }
 
 
-- (NSDictionary *) initializeSound:(NSString *)path {
-    
-    if (self.modPlayer) {
-        [self pause];
-    }
-    else {
-        self.modPlayer = [[MC_OMPT alloc] init];
-    }
-    
+- (NSDictionary *) loadFile:(NSString *)path {
 
 
-    
     self.modInfo = [self.modPlayer loadFile:path];
-    
-    NSArray *pathParts = [path componentsSeparatedByString:@"/"];
+     NSArray *pathParts = [path componentsSeparatedByString:@"/"];
     
     self.loadedFileName = [pathParts objectAtIndex:[pathParts count] - 1];
     
+    return self.modInfo;
+}
+
+- (NSDictionary *) initializeSound:(NSString *)path {
+    
+    isLoading = 1;
+    
+    if (self.modPlayer) {
+        [self loadFile:path];
+        isLoading = 0;
+
+        return self.modInfo;
+    }
+    
+    self.modPlayer = [[MC_OMPT alloc] init];
+    [self loadFile:path];
+
 
     AERenderer *renderer = [AERenderer new];
     self.output = [[AEAudioUnitOutput alloc] initWithRenderer:renderer];
@@ -240,8 +185,17 @@ void audioCallback(void *data, AudioQueueRef mQueue, AudioQueueBufferRef mBuffer
             return;
         }
 
+       
         float *leftBuffer  = (float*)bufferList->mBuffers[0].mData,
               *rightBuffer = (float*)bufferList->mBuffers[1].mData;
+        
+        
+        if (isLoading || isPaused) {
+            memset(leftBuffer,  0, bufferList->mBuffers[0].mDataByteSize);
+            memset(rightBuffer, 0, bufferList->mBuffers[0].mDataByteSize);
+            AERenderContextOutput(context, 1);
+            return;
+        }
 
         int32_t *currentStep = [self.modPlayer fillLeftBuffer:leftBuffer
                                               withRightBuffer:rightBuffer
@@ -278,128 +232,130 @@ void audioCallback(void *data, AudioQueueRef mQueue, AudioQueueBufferRef mBuffer
         NSLog(@"Dafuq?");
     }
 
+    isLoading = 0;
+
     
     return self.modInfo;
 }
 
 
 - (NSDictionary *) initializeSoundOld:(NSString *)path  {
-    
-    if (self.modPlayer) {
-    
-        [self pause];
-
-        AudioQueueStop(mAudioQueue, YES);
-        AudioQueueReset(mAudioQueue);
-        AudioQueueDispose(mAudioQueue, YES);
-    }
-    else {
-        BOOL success = [self initAudioSession];
-        if (! success) {
-            return nil;
-        }
-
-
-        self.modPlayer = [[MC_OMPT alloc] init];
-    }
-    
-    if (soundThread) {
-        [soundThread cancel];
-        // Give the current sound thread time to finish work.
-        [NSThread sleepForTimeInterval:.025];
-        
-        soundThread = nil;
-    }
-    
-    self.modInfo = [self.modPlayer loadFile:path];
-    
-    NSArray *pathParts = [path componentsSeparatedByString:@"/"];
-    
-    self.loadedFileName = [pathParts objectAtIndex:[pathParts count] - 1];
-    
-    AudioStreamBasicDescription mDataFormat;
-    UInt32 err;
-    
-    mDataFormat.mFormatID         = kAudioFormatLinearPCM;
-    mDataFormat.mFormatFlags      = kLinearPCMFormatFlagIsSignedInteger | kAudioFormatFlagIsPacked;
-    mDataFormat.mSampleRate       = PLAYBACK_FREQ;
-    mDataFormat.mBitsPerChannel   = 16;
-    mDataFormat.mChannelsPerFrame = 2;
-    mDataFormat.mBytesPerFrame    = (mDataFormat.mBitsPerChannel >> 3) * mDataFormat.mChannelsPerFrame;
-    mDataFormat.mFramesPerPacket  = 1;
-    mDataFormat.mBytesPerPacket   = mDataFormat.mBytesPerFrame;
-
-
-
-    err = AudioQueueNewOutput(&mDataFormat,
-                             audioCallback,
-                             CFBridgingRetain(self),
-                             CFRunLoopGetMain(),
-                             kCFRunLoopCommonModes,
-                             0,
-                             &mAudioQueue);
-    
-    bufferSize = SOUND_BUFFER_SAMPLE_SIZE / 2;
-//    printf("bufferSize == %i\n", bufferSize);
-
-    free(audioGenerationBuffer);
-//    free(audioVizBuffer);
-    free(vizGenerationBufferL);
-    free(vizGenerationBufferR);
-    free(mBuffers);
-
-    /* Allocate Audio generation buffer */
-    audioGenerationBuffer = (short int**)malloc(NUM_BUFFERS * sizeof(unsigned short int *));
-//    audioVizBuffer        = (short int**)malloc(NUM_BUFFERS * sizeof(unsigned short int *));
-    
-    vizGenerationBufferL  = (float **)malloc((NUM_BUFFERS) * sizeof(float *));
-    vizGenerationBufferR  = (float **)malloc((NUM_BUFFERS) * sizeof(float *));
-    
-    
-    /* Allocate Audio Queue buffers */
-    mBuffers = (AudioQueueBufferRef*) malloc(sizeof(AudioQueueBufferRef) * NUM_BUFFERS);
-    
-    soundGeneratorFlag = (int *)malloc(NUM_BUFFERS * sizeof(int));
-    
-    
-    numFrames = bufferSize / (2 * sizeof(int16_t));
-    self.numberOfFrames = numFrames;
-    
-    // Allocate buffers
-    for (int i = 0; i < NUM_BUFFERS; i++) {
-        /* Allocate buffers for AudioQueue */
-        AudioQueueBufferRef mBuffer;
-		AudioQueueAllocateBuffer(mAudioQueue, bufferSize, &mBuffer);
-		mBuffers[i] = mBuffer;
-        mBuffer->mAudioDataByteSize = bufferSize;
-
-        memset(mBuffer->mAudioData, 0, bufferSize);
-        
-        AudioQueueEnqueueBuffer(mAudioQueue, mBuffer, 0, NULL);
-        
-        
-        /* Allocate buffers for sound generation */
-        audioGenerationBuffer[i] = (short int*)malloc(bufferSize);
-//        audioVizBuffer[i]        = (short int*)malloc(bufferSize);
-        
-        vizGenerationBufferL[i]  = (float*)malloc(bufferSize);
-        vizGenerationBufferR[i]  = (float*)malloc(bufferSize);
-        
-        
-        /* set each value for the sound generator flag */
-        soundGeneratorFlag[i] = 0;
-    }
-
-    self.isPrimed = true;
-    
-//    soundThread = [[NSThread alloc] initWithTarget:self selector:@selector(generateAudioThread) object:nil];
-    
-    [soundThread start];
-    
-    soundGeneratorBufferIndex = 0;
-    soundPlayerBufferIndex = 0;
-    
-    return self.modInfo;
+//    
+//    if (self.modPlayer) {
+//    
+//        [self pause];
+//
+//        AudioQueueStop(mAudioQueue, YES);
+//        AudioQueueReset(mAudioQueue);
+//        AudioQueueDispose(mAudioQueue, YES);
+//    }
+//    else {
+//        BOOL success = [self initAudioSession];
+//        if (! success) {
+//            return nil;
+//        }
+//
+//
+//        self.modPlayer = [[MC_OMPT alloc] init];
+//    }
+//    
+//    if (soundThread) {
+//        [soundThread cancel];
+//        // Give the current sound thread time to finish work.
+//        [NSThread sleepForTimeInterval:.025];
+//        
+//        soundThread = nil;
+//    }
+//    
+//    self.modInfo = [self.modPlayer loadFile:path];
+//    
+//    NSArray *pathParts = [path componentsSeparatedByString:@"/"];
+//    
+//    self.loadedFileName = [pathParts objectAtIndex:[pathParts count] - 1];
+//    
+//    AudioStreamBasicDescription mDataFormat;
+//    UInt32 err;
+//    
+//    mDataFormat.mFormatID         = kAudioFormatLinearPCM;
+//    mDataFormat.mFormatFlags      = kLinearPCMFormatFlagIsSignedInteger | kAudioFormatFlagIsPacked;
+//    mDataFormat.mSampleRate       = PLAYBACK_FREQ;
+//    mDataFormat.mBitsPerChannel   = 16;
+//    mDataFormat.mChannelsPerFrame = 2;
+//    mDataFormat.mBytesPerFrame    = (mDataFormat.mBitsPerChannel >> 3) * mDataFormat.mChannelsPerFrame;
+//    mDataFormat.mFramesPerPacket  = 1;
+//    mDataFormat.mBytesPerPacket   = mDataFormat.mBytesPerFrame;
+//
+//
+//
+//    err = AudioQueueNewOutput(&mDataFormat,
+//                             audioCallback,
+//                             CFBridgingRetain(self),
+//                             CFRunLoopGetMain(),
+//                             kCFRunLoopCommonModes,
+//                             0,
+//                             &mAudioQueue);
+//    
+//    bufferSize = SOUND_BUFFER_SAMPLE_SIZE / 2;
+////    printf("bufferSize == %i\n", bufferSize);
+//
+//    free(audioGenerationBuffer);
+////    free(audioVizBuffer);
+//    free(vizGenerationBufferL);
+//    free(vizGenerationBufferR);
+//    free(mBuffers);
+//
+//    /* Allocate Audio generation buffer */
+//    audioGenerationBuffer = (short int**)malloc(NUM_BUFFERS * sizeof(unsigned short int *));
+////    audioVizBuffer        = (short int**)malloc(NUM_BUFFERS * sizeof(unsigned short int *));
+//    
+//    vizGenerationBufferL  = (float **)malloc((NUM_BUFFERS) * sizeof(float *));
+//    vizGenerationBufferR  = (float **)malloc((NUM_BUFFERS) * sizeof(float *));
+//    
+//    
+//    /* Allocate Audio Queue buffers */
+//    mBuffers = (AudioQueueBufferRef*) malloc(sizeof(AudioQueueBufferRef) * NUM_BUFFERS);
+//    
+//    soundGeneratorFlag = (int *)malloc(NUM_BUFFERS * sizeof(int));
+//    
+//    
+//    numFrames = bufferSize / (2 * sizeof(int16_t));
+//    self.numberOfFrames = numFrames;
+//    
+//    // Allocate buffers
+//    for (int i = 0; i < NUM_BUFFERS; i++) {
+//        /* Allocate buffers for AudioQueue */
+//        AudioQueueBufferRef mBuffer;
+//		AudioQueueAllocateBuffer(mAudioQueue, bufferSize, &mBuffer);
+//		mBuffers[i] = mBuffer;
+//        mBuffer->mAudioDataByteSize = bufferSize;
+//
+//        memset(mBuffer->mAudioData, 0, bufferSize);
+//        
+//        AudioQueueEnqueueBuffer(mAudioQueue, mBuffer, 0, NULL);
+//        
+//        
+//        /* Allocate buffers for sound generation */
+//        audioGenerationBuffer[i] = (short int*)malloc(bufferSize);
+////        audioVizBuffer[i]        = (short int*)malloc(bufferSize);
+//        
+//        vizGenerationBufferL[i]  = (float*)malloc(bufferSize);
+//        vizGenerationBufferR[i]  = (float*)malloc(bufferSize);
+//        
+//        
+//        /* set each value for the sound generator flag */
+//        soundGeneratorFlag[i] = 0;
+//    }
+//
+//    self.isPrimed = true;
+//    
+////    soundThread = [[NSThread alloc] initWithTarget:self selector:@selector(generateAudioThread) object:nil];
+//    
+//    [soundThread start];
+//    
+//    soundGeneratorBufferIndex = 0;
+//    soundPlayerBufferIndex = 0;
+//    
+    return @{};
 }
 
 // Create a thread to generate audio. Helps with skipping when the phone is hibernating, app is pushed to
@@ -418,8 +374,6 @@ void audioCallback(void *data, AudioQueueRef mQueue, AudioQueueBufferRef mBuffer
 //    printf("\n\nNew Thread \t\t%p\n", currentThread);
     
    float splitter = 32768.0f;
- 
-    
     
     while(1) {
    
@@ -429,8 +383,7 @@ void audioCallback(void *data, AudioQueueRef mQueue, AudioQueueBufferRef mBuffer
 //            printf("Exit thread \t\t%p\n", currentThread);
             [NSThread exit];
         }
-
-    
+   
         if (soundGeneratorFlag[soundGeneratorBufferIndex] == 0) {
 //            pthread_mutex_lock(&audio_mutex);
           
@@ -557,7 +510,7 @@ void interruptionListenerCallback (void *inUserData, UInt32 interruptionState ) 
 - (void) play {
 
     [self.output start:nil];
-    
+    isPaused = false;
     self.isPlaying = true;
     [self updateInfoCenter];
 
@@ -570,15 +523,18 @@ void interruptionListenerCallback (void *inUserData, UInt32 interruptionState ) 
 }
 
 - (void) pause {
-    [self.output stop];
-
+//    [self.output stop];
+    isPaused = true;
     self.isPlaying = false;
 
 }
 
 - (void) resume {
     [self.output start:nil];
+    isPaused = false;
     self.isPlaying = true;
+    [self updateInfoCenter];
+
 }
 
 - (void) dealloc {
